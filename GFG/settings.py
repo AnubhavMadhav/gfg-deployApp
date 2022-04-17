22b77b6881d56d58628a2a7d6775ddddc40da415
Anubhav Madhav <201851024@iiitvadodara.ac.in>
2022-04-17 16:53:32 +0530

    deploy 2

--- GFG/settings.py
+++ GFG/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#7#l1ll=41*fmudntod)@6w%!-ctirshl&=jou6f9nk8wpbfn*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
